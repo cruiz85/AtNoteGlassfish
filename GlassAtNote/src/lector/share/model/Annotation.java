ac0e27d02ac44ef5215443b5b4464e3972b83c14
Cesar <Cesar@Cesar-PC>
2012-12-13 14:53:35 +0100

    entry_annotation

--- GlassAtNote/src/lector/share/model/Annotation.java
+++ GlassAtNote/src/lector/share/model/Annotation.java
@@ -45,7 +45,7 @@ public class Annotation implements Serializable {
 	private Integer pageNumber;
 
 	@ManyToMany(mappedBy="annotations",cascade = CascadeType.REFRESH)
-	@JoinTable(name = "entry_annotation", joinColumns = { @JoinColumn(name = "annotationId", referencedColumnName = "id") }, inverseJoinColumns = { @JoinColumn(name = "tagId", referencedColumnName = "id") })
+	@JoinTable(name = "entry_annotation", joinColumns = { @JoinColumn(name = "annotations_id", referencedColumnName = "id") }, inverseJoinColumns = { @JoinColumn(name = "tags_id", referencedColumnName = "id") })
 	private List<Tag> tags;
 
 	@Temporal(javax.persistence.TemporalType.DATE)
